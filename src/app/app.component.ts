fafd9f669b7d3c751db10ef033a110eaff891086
juanotturi <juanotturi@gmail.com>
2025-07-12 00:14:20 -0300

    Se mejoro el nav-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { NavigationService } from './services/navigation.service';
 import { RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, NgClass ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'simuladorfutbol-front';
+  currentRoute = '';
 
   constructor(
     private router: Router,
     private navigationService: NavigationService
-  ) {}
+  ) {
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        this.currentRoute = e.urlAfterRedirects;
+    });
+  }
+
+  isActive(route: string): boolean {
+    return this.currentRoute.startsWith(route);
+  }
 
   goToHome() {
     this.router.navigate(['/home']);
